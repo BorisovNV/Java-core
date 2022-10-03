3a38721c4bf44f5d3845cb4f91b3d2d7a1f5ea91
Nikolay.Borisov <piterskij84@gmail.com>
2022-10-03 21:03:52 +0300

    hw2

--- src/main/java/Lesson2/MyArrayDataException.java
+++ src/main/java/Lesson2/MyArrayDataException.java
@@ -9,5 +9,4 @@ public class MyArrayDataException extends Exception {
         this.j = j;
 
     }
-
 }
--- src/main/java/Lesson2/MyArraySizeException.java
+++ src/main/java/Lesson2/MyArraySizeException.java
@@ -5,4 +5,5 @@ public class MyArraySizeException extends Exception {
         super(msg);
         this.msg = msg;
     }*/
+
 }
